00616d7cd772eb2492c6a0cfc56a19855c321ebc
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-11-04 10:29:23 +0600

    47-2

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,26 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './components/Root/Root';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+
+import Dashboard from './components/Dashboard/Dashboard';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        // element: <Home></Home>
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard></Dashboard>,
+      }
+    ]
   },
 ]);
 
